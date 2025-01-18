30cd519effba389a206c07f7ebe6c937b8a75066
ramiroebeyo <ramiroebeyo@gmail.com>
2025-01-18 12:06:05 -0300

    prueba2

--- Pagina-web-proyecto/partials/topNav.php
+++
@@ -1,4 +0,0 @@
-<div class="contentTopNav">
-    <a class="navicon" href="" id="Navicon"><i class="fa fa-navicon"></i></a>
-    <a class="power-off" href="database/logout.php"><i class="fa fa-power-off"></i> Log-out</a>
-</div>
