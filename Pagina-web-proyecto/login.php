0e22858efb45f3ccb3dbcf812055e94e8889c884
ramiroebeyo <ramiroebeyo@gmail.com>
2025-01-18 12:02:25 -0300

    prueba

---
+++ Pagina-web-proyecto/login.php
@@ -0,0 +1,87 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user'])) header('location: dashboard.php');
+
+    $error_message = '';
+
+    if($_POST){
+        include('database/connection.php'); 
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //$query = 'SELECT * FROM users WHERE users.email="'. $username .'" AND users.password= "'. $password .'"';
+        //$stmt = $conn->prepare($query);
+        //$stmt->execute();
+        
+        
+
+        //if($stmt->rowCount() > 0){
+            //$stmt->setFetchMode(PDO::FETCH_ASSOC);
+            //$user = $stmt->fetchAll()[0];
+            //$_SESSION['user'] = $user;
+
+            //header('Location: dashboard.php');
+        //} else $error_message = 'El nombre de usuario o la contraseña no son correctos';
+        $stmt = $conn->prepare("SELECT * FROM users");
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        
+        $users = $stmt->fetchAll();
+
+        $user_exist = 0;
+        foreach($users as $user){
+            $upass = $user['password'];
+            if($upass == $password){
+                $user_exist +=1;
+                $_SESSION['user'] = $user;
+                break;
+            }
+        }
+        
+        if($user_exist > 0) header('location: dashboard.php');
+        else $error_message = 'El nombre de usuario o la contraseña no son correctos';
+
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>IMS - Login Page</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body id="loginBody">
+    <?php
+        if(!empty($error_message)) { ?>
+    <div id="errorMessage">
+        <p>
+            Error: <?= $error_message ?> 
+        </p>
+    <?php } ?>
+    </div>
+    <div class="container"> 
+        <div class='loginHeader'>
+                <h1>IMS</h1>
+                <p>Inventory Management System</p>
+        </div>
+        <div class='loginBody'>
+            <form action='login.php' method='POST'>
+                <div class='loginInputsContainer'>
+                    <label for="">Username</label>
+                    <input placeholder="username" name="username" type="text" />
+                </div>
+                <div class='loginInputsContainer'>
+                    <label for="">Password</label>
+                    <input placeholder="password" name="password" type="password" />
+                </div>
+                <div class="loginButtonContainer">
+                    <button type="submit">Login</button>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
